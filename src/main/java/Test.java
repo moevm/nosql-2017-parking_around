14eb8fd87052aa934accd48be828dbfbddf5e466
Talanov Stanislav <enotpoloskyn1470@gmail.com>
2017-09-21 01:02:15 +0300

    geodesic distance between two points

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,7 +2,9 @@
  * Created by Stanislav on 21.09.2017.
  */
 import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+import org.neo4j.graphdb.spatial.Coordinate;
 
 import java.io.File;
 
@@ -11,5 +13,17 @@ public class Test {
         GraphDatabaseFactory graphDbFactory = new GraphDatabaseFactory();
         GraphDatabaseService graphDb = graphDbFactory.newEmbeddedDatabase(
                 new File("data/cars"));
+        String resultString;
+        Coordinate domitry = new Coordinate(59.991298, 30.319191);
+        Coordinate university = new Coordinate(59.972128, 30.323983);
+        try {
+            Transaction tx = graphDb.beginTx();
+            resultString = graphDb.execute(
+                    "WITH point({ x: "+domitry.getCoordinate().get(0)+", y:"+domitry.getCoordinate().get(0)+", crs: 'cartesian' }) AS p1," +
+                    " point({ x: "+university.getCoordinate().get(0)+", y:"+university.getCoordinate().get(0)+", crs: 'cartesian' }) AS p2 RETURN distance(p1,p2) AS dist").resultAsString();
+            System.out.println(resultString);
+        } catch (Exception e) {
+            System.err.println("Trouble with execute: " + e.getMessage());
+        }
     }
 }
